5be6c7ddac69fc50fb437044abbdc53bfbc720ff
Atabek <atabekarapov14@gmail.com>
2020-11-02 12:27:49 +0600

    Week2_Task2

---
+++ Task2.py
@@ -0,0 +1,88 @@
+
+
+#  Homework ---> ATABEK
+#  Part 1
+# 1)
+# a = 'Amantay! '
+# b = 'Alina! '
+# c = 'Tilek! '
+# string = 'Hello '
+# lunch = 'Come for lunch today.'
+# print(string + a + lunch)
+# print(string + b + lunch)
+# print(string + c + lunch)
+
+# 2)
+# spisok= ['Amantay', 'Alina', 'Tilek', 'Syimyk', 'Artur']
+# new_spisok = spisok.copy()
+# new_spisok[2] = 'Bektur'
+# a = "Tilek cannot come today! "
+# b = "Hello, "
+# lunch = 'Come for lunch today.'
+# print(b + str(spisok) + lunch)
+# print(a + b + str(new_spisok) + lunch)
+
+# 3)
+# spisok= ['Amantay', 'Alina', 'Tilek', 'Syimyk', 'Artur']
+# spisok.insert(0, 'Amanali')
+# spisok.insert(2, 'Hakim')
+# spisok.insert(7, 'Nurzhan')
+# # print(spisok)
+# b = "Hello, "
+# lunch = 'Come for lunch today.'
+# print(b + str(spisok) + lunch)
+
+# 4)   
+# spisok = ['Syimyk', 'Artur']
+# spisok.insert(0, 'Amanali')
+# spisok.insert(2, 'Hakim')
+# spisok.insert(7, 'Nurzhan')
+# i = spisok.index('Syimyk')
+# e = spisok[-1]
+# d = spisok[-3]
+# removed_item = spisok.pop(i)
+# spisok.remove(e)
+# spisok.remove(d)
+# print(' Unfortunetely you guys are not coming: ' + e + ' ' + d)
+# print(' Only you guys are coming: ' + str(spisok))
+
+
+# 5)
+# suitcase = ['phone', 'laptop', 'napkin', 'glasses', 'shampoo']
+# suitcase.append('toothbrash')
+# suitcase.append('pen')
+# suitcase.pop()
+# suitcase.insert(0,'notebook')
+# print(suitcase)
+
+# First Tasks Homework ---> ATABEK  (Metody Spiskov(T1))
+# 1) 
+# name = ['John', 'Jessica', 'Jerry', 'Tom', 'Ben']
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+# 2)
+# name = ['Mers', 'Lexus', 'BMW']
+# bold = 'My favorite car is '
+# bold2 = 'I like ' 
+# bold3 = 'cars!'
+# print(bold + name[0])
+# print(bold2 + name[1] + ' ' + bold3)
+
+# 3)
+# numbers = ['str1 ', 'str2 ']
+# a = (numbers[ -1 ])
+# b = (numbers[ 0 ])
+# print(a + b)
+
+# 4)
+# spisok = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+# length = len(spisok)
+# number = length // 2
+# slice1 = spisok[0:number]
+# slice2= spisok[number::]
+# answer = slice2 + slice1
+# print (answer)
